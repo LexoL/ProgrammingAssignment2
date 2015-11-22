5bb01615bc09dc4d3b0370ad85d7b464a5282a24
LexoL <alexander.tulupyev@gmail.com>
2015-11-23 01:09:14 +0300

    The code is developed and tested, meets the specs.

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,49 @@
+## cachematrix.R
 ## This script is developed according to the Coursera course requirements of
 ## R Programming -- Programming Assignment 2: Lexical Scoping
-## ADD BFIEF DESCRIPTION of WHAT thees
-## functions do
-## functions do
-## functions do
+## -------------------------------------------------------------------------
+## This script contains two functions makeCacheMatrix and cacheSolve 
+## that provides a way to create a matrix as well as calculate and cache 
+## its inverse.
+## If the matrix does not change, the inverse is calculated just once
+## and then is cached. Next time cacheSolve will return the cached
+## value of the inverse. If the matrix changes, the inverse will
+## again be calculated for the first time. 
+## -------------------------------------------------------------------------
+## USAGE:
+##
+## > source("cachematrix.R")
+## > mdat <- matrix(c(2, -1,   8, 0.5), nrow = 2, ncol = 2, byrow = TRUE)
+## > m <- makeCacheMatrix(mdat)
+## > cinv <- cacheSolve(m)
+## > cinv
+##             [,1]      [,2]
+## [1,]  0.05555556 0.1111111
+## [2,] -0.88888889 0.2222222
+## > m$getInversedMatrix()
+##             [,1]      [,2]
+## [1,]  0.05555556 0.1111111
+## [2,] -0.88888889 0.2222222
+## > m2 <- makeCacheMatrix(m$getInversedMatrix())
+## > cinv2 <- cacheSolve(m2)
+## > cinv2
+##      [,1] [,2]
+## [1,]    2 -1.0
+## [2,]    8  0.5
+## > m2$getInversedMatrix()
+##      [,1] [,2]
+## [1,]    2 -1.0
+## [2,]    8  0.5
 
-## Write a short comment describing this function
+## IMPLEMENTATION:
+
+## makeCacheMatrix creates a matrix and returns a list of methods, to operate
+## this matrix and its cached inverse.
+## get retrieves the matrix,
+## set saves a new matrix and  assignes null to the cached inverse,
+## getInversedMatrix retrieves the inverse of the matrix or null if the 
+## inverse is not calculated yet,
+## setInversedMatrix saves (caches) the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
         inversedMatrix <- NULL
@@ -29,7 +67,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## caches and returns the calculated inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         inv <- x$getInversedMatrix()
         if(!is.null(inv)) {
                 # getting cached inverse of matrix x
